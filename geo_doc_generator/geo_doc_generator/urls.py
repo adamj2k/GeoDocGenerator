3de2d2214983e16a95c55d3b44db17143ea671c2
adamjk <adamjawor@gmail.com>
2023-01-15 20:38:05 +0100

    add urls path to apps in project

--- geo_doc_generator/geo_doc_generator/urls.py
+++ geo_doc_generator/geo_doc_generator/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("", admin.site.urls),
+    path("/admin", admin.site.urls),
+    path("", include('geodetic_work.urls')),
+    path("geodetictools/", include('geodetic_tools.urls')),
+    path("geodetictechnicaldoc/", include('geodetic_technical_documentation.urls')),
+    
 ]
