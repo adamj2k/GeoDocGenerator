9dc6f93cdbd3d04e64472c0b79934dee52a452c2
Adam <adamjawor@gmail.com>
2023-09-22 14:18:49 +0200

    update settings and requirements

--- geo_doc_generator/geo_doc_generator/settings.py
+++ geo_doc_generator/geo_doc_generator/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "phonenumber_field",
     "crispy_forms",
+    "crispy_bootstrap4",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
